d0b0001c5e41b2b01afa5e47ebdf958d054341cc
OHE64 <deut.nemless.deut@gmail.com>
2026-01-19 23:58:56 +0100

    add Tests

---
+++ Tests.Worldserver/ProjectActionsTestsOHE.cs
@@ -0,0 +1,24 @@
+﻿using Tests.Worldserver.Base;
+using Apps.Worldserver.Actions;
+using Apps.Worldserver.Models.Projects.Request;
+
+namespace Tests.Worldserver;
+
+[TestClass]
+public class ProjectActionsTestsOHE : TestBase
+{
+    [TestMethod]
+    public async Task SearchProjects_ReturnsProjects()
+    {
+		// Arrange
+		var actions = new ProjectActions(InvocationContext);
+        var request = new GetProjectRequest { ProjectId= "442840" };
+
+        // Act
+        var result = await actions.GetProject(request);
+
+        // Assert
+        PrintJsonResult(result);
+        Assert.IsNotNull(result);
+    }
+}
---
+++ Tests.Worldserver/TaskActionsTestsOHE.cs
@@ -0,0 +1,28 @@
+﻿using Tests.Worldserver.Base;
+using Apps.Worldserver.Actions;
+using Apps.Worldserver.Models.Tasks.Request;
+
+namespace Tests.Worldserver;
+
+[TestClass]
+public class TaskActionsTestsOHE : TestBase
+{
+    [TestMethod]
+    public async Task GetTask_ReturnsTask()
+    {
+        var actions = new TaskActions(InvocationContext, null!);
+
+        var request = new GetTaskRequest 
+        { 
+            TaskId = "2694497" 
+        };
+
+        var result = await actions.GetTask(request);
+
+        PrintJsonResult(result);
+        Assert.IsNotNull(result);
+
+        // Falls du weißt, welche Felder zurückkommen sollen:
+        // Assert.AreEqual("2694497", result.Id);
+    }
+}
